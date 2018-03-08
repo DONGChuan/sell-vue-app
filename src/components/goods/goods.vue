8a9d3b789a3ae725b4fe34198ce6793b9a839e3e
Chuan DONG <chuan.dong@tradingreason.com>
2018-03-08 09:48:51 +0800

    添加购物车组件

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -35,15 +35,20 @@
         </li>
       </ul>
     </div>
+    <shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import BScroll from 'better-scroll';
+import Shopcart from 'components/shopcart/shopcart';
 
 const ERR_OK = 0;
 
 export default {
+    components: {
+        Shopcart
+    },
     props: {
         seller: {
             type: Object
