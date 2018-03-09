3222b420df878f13026e131a2f94913b1a1662e8
Chuan DONG <chuan.dong@tradingreason.com>
2018-03-09 17:28:23 +0800

    添加商品评价标签

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -31,6 +31,12 @@
           <h1 class="title">商品信息</h1>
           <p class="text">{{ food.info }}</p>
         </div>
+        <split/>
+        <!-- 商品评价 -->
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect :selectType="selectType" :onlyContent="onlyContent" :desc="desc" :ratings="food.ratings"/>
+        </div>
       </div>
     </div>
   </transition>
@@ -41,11 +47,17 @@ import BScroll from 'better-scroll';
 import Vue from 'vue';
 import cartcontrol from 'components/cartcontrol/cartcontrol';
 import split from 'components/split/split';
+import ratingselect from 'components/ratingselect/ratingselect';
+
+// const POSITIVE = 0;
+// const NEGATIVE = 1;
+const ALL = 2;
 
 export default {
     components: {
         cartcontrol,
-        split
+        split,
+        ratingselect
     },
     props: {
         food: {
@@ -54,12 +66,21 @@ export default {
     },
     data() {
         return {
-            showFlag: false
+            showFlag: false,
+            selectType: ALL,
+            onlyContent: true,
+            desc: {
+                all: '全部',
+                positive: '推荐',
+                negative: '吐槽'
+            }
         };
     },
     methods: {
         show() {
             this.showFlag = true;
+            this.selectType = ALL;
+            this.onlyContent = true;
             this.$nextTick(() => {
                 if (!this.scroll) {
                     this.scroll = new BScroll(this.$refs.food, {
@@ -186,4 +207,12 @@ export default {
         padding 0 8px
         font-size 12px
         color rgb(77,85,93)
+    .rating
+      padding-top 18px
+      margin-left 18px
+      .title
+        line-height 14px
+        margin-bottom 6px
+        font-size 14px
+        color rgb(7,17,27)
 </style>
